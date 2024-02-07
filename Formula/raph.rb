9571df40f2852ff79eea467893d44cb4e97184fd
Nick Hassan <nick@hassan.host>
2024-02-07 22:21:00 +1030

    Update raph.rb

--- Formula/raph.rb
+++ Formula/raph.rb
@@ -13,7 +13,7 @@ class Raph < Formula
   # end
 
   bottle do
-    root_url "https://github.com/n-hass/homebrew-tools/releases/download/v0.1.2"
+    root_url "https://github.com/n-hass/raph/releases/download/v0.1.2"
     rebuild 1
     sha256 cellar: :any_skip_relocation, sonoma: "7fe291631a81807378a3a227a94cc906a4c18159e4e59cf56ab3cc9700348ed9"
   end
